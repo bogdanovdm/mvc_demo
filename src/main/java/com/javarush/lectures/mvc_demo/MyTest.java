dcbad5d0891a2b69392ee888bf6cbce28c3afd00
Dmitriy Bogdanov <eiffil@bk.ru>
2020-05-19 01:56:51 +0300

    Убрали ужасное форматирование кода

--- src/main/java/com/javarush/lectures/mvc_demo/MyTest.java
+++ src/main/java/com/javarush/lectures/mvc_demo/MyTest.java
@@ -1,12 +1,9 @@
 package com.javarush.lectures.mvc_demo;
 
 public class MyTest {
-
+    // Точка входа в программу
     public static void main(String[] args) {
-
         // Внесли изменение №2
         System.out.println("Hello World");
-
     }
-
 }
