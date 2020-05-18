33ea36899759b56db35655fcb507a2052ed31c25
Dmitriy Bogdanov <eiffil@bk.ru>
2020-05-19 01:45:48 +0300

    Внесли изменение №2

--- src/main/java/com/javarush/lectures/mvc_demo/MyTest.java
+++ src/main/java/com/javarush/lectures/mvc_demo/MyTest.java
@@ -2,6 +2,7 @@ package com.javarush.lectures.mvc_demo;
 
 public class MyTest {
     public static void main(String[] args) {
+        // Внесли изменение №2
         System.out.println("Hello world");
     }
 }
