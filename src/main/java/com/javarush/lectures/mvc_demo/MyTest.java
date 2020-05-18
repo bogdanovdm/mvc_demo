6defe4a4ec6e1870004284d717fc6c2f2e955001
Dmitriy Bogdanov <eiffil@bk.ru>
2020-05-19 01:48:46 +0300

    Отформатировали код, чтобы стало красиво

--- src/main/java/com/javarush/lectures/mvc_demo/MyTest.java
+++ src/main/java/com/javarush/lectures/mvc_demo/MyTest.java
@@ -1,8 +1,12 @@
 package com.javarush.lectures.mvc_demo;
 
 public class MyTest {
+
     public static void main(String[] args) {
+
         // Внесли изменение №2
         System.out.println("Hello World");
+
     }
+
 }
